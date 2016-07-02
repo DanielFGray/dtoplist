389bb00de2785409bea5489c0bc4dcd2ae4923bc
DanielFGray <DanielFGray@gmail.com>
2016-07-02 03:09:05 -0500

    fix for #3

--- app/components/NickList.jsx
+++ app/components/NickList.jsx
@@ -3,12 +3,33 @@ import React, { Component, PropTypes } from 'react';
 export default class NickList extends Component {
   static propTypes = { nicks: PropTypes.array, clickNick: PropTypes.func };
 
+  constructor(props) {
+    super(props);
+    this.filterChange = this.filterChange.bind(this);
+    this.state = { nickFilter: '' };
+  }
+
+  filterChange(e) {
+    this.setState({ nickFilter: e.target.value });
+  }
+
   render() {
+    const nicks = this.props.nicks
+      .filter(n => n.toLowerCase().indexOf(this.state.nickFilter.toLowerCase()) > -1);
+
     return (
       <div style={{ display: 'block-inline' }} className="pull-right text-right">
-        <div>{this.props.nicks.length} users shown</div>
+        <div className="input-control">
+          <input
+            type="text"
+            className="input-control"
+            placeholder="search for a name"
+            value={this.state.nickFilter}
+            onChange={this.filterChange}
+          />
+        </div>
         <ul className="list-unstyled">
-          {this.props.nicks.map(e => (
+          {nicks.map(e => (
             <li key={e}>
               <a onClick={this.props.clickNick}>{e}</a>
             </li>

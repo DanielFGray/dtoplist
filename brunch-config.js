75dd34d2e0012c46effb7c941423e3fba70c416a
DanielFGray <DanielFGray@gmail.com>
2016-07-03 18:37:13 -0500

    brunch config formatting

--- brunch-config.js
+++ brunch-config.js
@@ -1,15 +1,20 @@
-module.exports = {
-  files: {
-    javascripts: {
-      joinTo: {
-        'vendor.js': /^(?!app)/,
-        'app.js': /^app/
+module.exports =
+  { files:
+    { javascripts:
+      { joinTo:
+        { 'vendor.js': /^(?!app)/
+        , 'app.js': /^app/
+        }
       }
-    },
-    stylesheets: { joinTo: 'app.css' }
-  },
-
-  plugins: {
-    babel: { presets: [ 'es2015', 'stage-0', 'react' ] }
-  }
-};
+    , stylesheets: { joinTo: 'app.css' }
+    }
+  , plugins:
+    { babel:
+      { presets:
+        [ 'es2015'
+        , 'stage-0'
+        , 'react'
+        ]
+      }
+    }
+  };
